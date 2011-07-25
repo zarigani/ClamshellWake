4ba5b48142aabb8c0f0753a6f7a587552c361dca
zarigani <zariganigamiteita@gmail.com>
2011-07-25 20:29:34 +0900

    tr MyDriver ClamshellWake

--- ClamshellWake.cpp
+++ ClamshellWake.cpp
@@ -10,14 +10,14 @@ OSDefineMetaClassAndStructors(com_bebekoubou_driver_ClamshellWake, IOService)
 #define super IOService
 bool com_bebekoubou_driver_ClamshellWake::start(IOService *provider)
 {
-    IOLog("MyDriver: Starting\n");
+    IOLog("ClamshellWake: Starting\n");
     com_bebekoubou_driver_ClamshellWake::send_event(kIOPMDisableClamshell); 
     return super::start(provider);
 }
 
 void com_bebekoubou_driver_ClamshellWake::stop(IOService *provider)
 {
-    IOLog("MyDriver: Stopping\n");
+    IOLog("ClamshellWake: Stopping\n");
     com_bebekoubou_driver_ClamshellWake::send_event(kIOPMEnableClamshell | kIOPMAllowSleep);
     super::stop(provider);
 }
@@ -26,17 +26,17 @@ void com_bebekoubou_driver_ClamshellWake::stop(IOService *provider)
 IOReturn com_bebekoubou_driver_ClamshellWake::message(UInt32 type, IOService * provider, void * argument)
 {	
 	if (type == kIOPMMessageClamshellStateChange) {
-		IOLog("MyDriver: Clamshell State Changed\n");
+		IOLog("ClamshellWake: Clamshell State Changed\n");
 		
         // If lid was opened
 		if ((argument && kClamshellStateBit) & (!lastLidState)) {
-			IOLog("MyDriver: kClamshellStateBit set - lid was opened\n");
+			IOLog("ClamshellWake: kClamshellStateBit set - lid was opened\n");
 			lastLidState = true;
 			com_bebekoubou_driver_ClamshellWake::send_event(kIOPMClamshellOpened);
 			
         // If lastLidState is true - lid closed
 		} else if (lastLidState) {
-			IOLog("MyDriver: kClamshellStateBit not set - lid was closed \n");
+			IOLog("ClamshellWake: kClamshellStateBit not set - lid was closed \n");
 			lastLidState = false;
 			com_bebekoubou_driver_ClamshellWake::send_event(kIOPMDisableClamshell); 
 		}
@@ -53,22 +53,22 @@ bool com_bebekoubou_driver_ClamshellWake::send_event(UInt32 msg)
 	IOReturn		ret=kIOReturnSuccess;
 	
     // warning is no problem.(format '%X' expects type 'unsigned int', but argument 2 has type 'UInt32')
-    IOLog("MyDriver:   Sending event 0x%X\n", msg);
+    IOLog("ClamshellWake:   Sending event 0x%X\n", msg);
 	
 	root = getPMRootDomain();
     if (!root) {
-        IOLog("MyDriver:   Fatal error could not get RootDomain.\n");
+        IOLog("ClamshellWake:   Fatal error could not get RootDomain.\n");
         return false;
     }
 	
 	ret = root->receivePowerNotification(msg);
 	
-	IOLog("MyDriver:   root returns %d\n", ret);
+	IOLog("ClamshellWake:   root returns %d\n", ret);
 	
 	if(ret!=kIOReturnSuccess)
-		IOLog("MyDriver:   Error sending event\n");
+		IOLog("ClamshellWake:   Error sending event\n");
 	else
-		IOLog("MyDriver:   Message sent to root\n");
+		IOLog("ClamshellWake:   Message sent to root\n");
 	
 	return true;
 }
